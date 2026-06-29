278195c627be6777e91b97271db203353707a321
Salman Farshi <sfarshi222@gmail.com>
2026-06-29 22:33:53 +0600

    fixed: problem footer pera

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
    <section className='bg-background mt-24'>
     <div className=" py-11 px-4 md:px-0 flex flex-col md:flex-row gap-4 justify-between items-center max-w-330 mx-auto">
         <img src={Logo} alt="" />
-        <p className='text-secondary text-[16px] font-light'>Copyright © 2023 Wizia. All rights reserved.</p>
+        <p className='text-secondary text-sm md:text-[16px] font-light '>Copyright © 2023 Wizia. All rights reserved.</p>
 
 
     </div>
